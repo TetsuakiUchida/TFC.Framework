f6ba2e47ece04a25a32da2b3daa95c14bb1f9350
Tetsuaki.Uchida <tetuaki@timedia.co.jp>
2016-09-09 17:02:11 +0900

    ToEnumerable

---
+++ TFC.Framework.Core/EnumerableUtil.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace TFC.Framework.Core
+{
+    /// <summary>
+    /// IEnumerableに関するユーティリティ
+    /// </summary>
+    public static class EnumerableUtil
+    {
+        public static IEnumerable<T> ToEnumerable<T>(this T value)
+        {
+            return new T[] { value };
+        }
+    }
+}
