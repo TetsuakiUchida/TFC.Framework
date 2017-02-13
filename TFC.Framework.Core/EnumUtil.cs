8c22672413933b28ec1b410d43baa949030d7175
Tetsuaki.Uchida <tetuaki@timedia.co.jp>
2017-02-13 15:26:43 +0900

    EnumUtilの追加

---
+++ TFC.Framework.Core/EnumUtil.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace TFC.Framework.Core
+{
+    /// <summary>
+    /// Enumに関するユーティリティ
+    /// </summary>
+    public static class EnumUtil
+    {
+        public static IEnumerable<T> GetValues<T>() where T : struct
+        {
+            var t = typeof(T);
+            if (!t.IsEnum) throw new ArgumentException($"T is not typeof enum:{t.ToString()}");
+
+            foreach (T item in Enum.GetValues(t))
+            {
+                yield return item;
+            }
+        }
+    }
+}
